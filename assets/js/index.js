64ec931c1cd474bd05a92c4fb3e9a1692aa055be
lucmunoz <lucas.munoz.vasquez@gmail.com>
2024-02-11 23:42:46 -0300

    grafico

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,20 +2,81 @@ let valorDolar = 0;
 let valorUf = 0;
 let valorEuro = 0;
 let valorBitcoin = 0;
-let apiStatus
+let myChart
 
+const $moneyAmountInputBox = document.getElementById("input-box");
+const $moneyTypeSelectBox = document.getElementById("money-select");
 
-const $inputBox = document.getElementById("input-box");
-const $selectBox = document.getElementById("money-select");
-const $Button = document.getElementById("button");
-$Button.addEventListener("click", getValueAndProcess);
-const $resultText = document.getElementById("result");
+$moneyTypeSelectBox.addEventListener("change", function(){
+    getDataAndCreateChart(this.value)
+});
+
+const $convertButton = document.getElementById("button");
+$convertButton.addEventListener("click", getValueAndProcess);
+const $convertedAmountText = document.getElementById("result");
 const $chart = document.getElementById("myChart");
+let ctx = document.getElementById("myChart").getContext('2d');
+
+getDataAndCreateChart($moneyTypeSelectBox.value)
+
+async function getDataAndCreateChart(tipo){  
+    try {
+        const res = await fetch("https://mindicador.cl/api/"+tipo+"/");
+        const datos = await res.json();
+
+
+        createChart(datos);
+    }
+    catch (error) {
+        alert("Error. Api caída");
+    }
+}
 
 
 
-window.addEventListener("load",renderGrafica);
+async function createChart(datos){
 
+    const labelsTemp = datos.serie.map((dato) => {
+        const fecha = dato.fecha.split("T")[0];
+        return fecha;
+    });
+
+    labels = labelsTemp.splice(0,10).reverse();
+    //console.log (labels)
+
+    const valuesTemp = datos.serie.map((dato) => {
+        const magnitud = dato.valor;
+        return Number(magnitud);
+    });
+
+    const values = valuesTemp.splice(0,10).reverse();
+    //console.log (values)
+
+    const datasets = [
+            {
+                label: "Valor dolar - 10 últimos días",
+                borderColor: "rgb(255, 99, 132)",
+                data: values
+            }
+        ];
+
+    const chartData = {labels, datasets }
+
+    const config = {
+        type: "line",
+        data: chartData
+    };
+
+    if(myChart){
+        myChart.destroy();
+    }
+    
+    myChart = new Chart(ctx, config);
+}
+
+
+
+//window.addEventListener("load",renderGrafica);
 
 async function getMoneyIndicators() {
     try {
@@ -32,26 +93,26 @@ async function getMoneyIndicators() {
     } 
     catch (error) {
         alert("Error. Api caída");
-        $inputBox.value="";
-        $inputBox.placeholder = "monto en pesos chilenos (CLP)";
-        $resultText.innerHTML =  "..."; 
+        $moneyAmountInputBox.value="";
+        $moneyAmountInputBox.placeholder = "monto en pesos chilenos (CLP)";
+        $convertedAmountText.innerHTML =  "..."; 
 
     }
 }
 
 function convertData(){
 
-let valorTemp = parseInt($inputBox.value);
+let valorTemp = parseInt($moneyAmountInputBox.value);
 
       if(isNaN(valorTemp)) {
         alert("Porfavor, debe ingresar un valor numerico válido. No utilice letras ni puntos.");
-        $inputBox.value="";
-        $inputBox.placeholder = "monto en pesos chilenos (CLP)";
-        $resultText.innerHTML =  "...";
+        $moneyAmountInputBox.value="";
+        $moneyAmountInputBox.placeholder = "monto en pesos chilenos (CLP)";
+        $convertedAmountText.innerHTML =  "...";
         return;
       }
 
-    let tipoMoneda = $selectBox.value
+    let tipoMoneda = $moneyTypeSelectBox.value
 
     if(tipoMoneda==="dolar"){
         valorTemp = parseFloat(valorTemp/valorDolar).toFixed(3);
@@ -65,68 +126,13 @@ let valorTemp = parseInt($inputBox.value);
     else if(tipoMoneda==="bitcoin"){
         valorTemp = parseFloat(valorTemp/valorBitcoin).toFixed(3);
     }
-    $resultText.innerHTML =  valorTemp;
+    $convertedAmountText.innerHTML =  valorTemp;
 
 }
 
 
 
 function getValueAndProcess (){
-
     getMoneyIndicators();
-
 }
 
-async function getAndCreateDataToChart() {
-    const res = await fetch("https://mindicador.cl/api/dolar/");
-    const datos = await res.json();
-
-    console.log (datos)
-    const labelsTemp = datos.serie.map((dato) => {
-        const fecha = dato.fecha.split("T")[0];
-        return fecha;
-    });
-
-    labels = labelsTemp.splice(0,10).reverse();
-
-    console.log (labels)
-   
-    const dataTemp = datos.serie.map((dato) => {
-    const magnitud = dato.valor;
-    return Number(magnitud);
-    });
-
-    data=dataTemp.splice(0,10).reverse();
-
-
-    console.log (data)
-
-    
-    const datasets = [
-    {
-    label: "Valor histórico dolar - 10 últimos días",
-    borderColor: "rgb(255, 99, 132)",
-    data
-    }
-    ];
-
-    return { labels, datasets };
-}
-
-
-
-
-
-async function renderGrafica() {
-
-const data = await getAndCreateDataToChart();
-
-const config = {
-type: "line",
-data
-};
-
-
-myChart.style.backgroundColor = "white";
-new Chart(myChart, config);
-}
