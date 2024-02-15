365582f851e5f4368777b3ad80ca8712e702064e
lucmunoz <lucas.munoz.vasquez@gmail.com>
2024-02-14 22:40:12 -0300

    cmentarios

--- assets/js/index.js
+++ assets/js/index.js
@@ -109,27 +109,55 @@ const convertAndShowCurrency = (data) =>{
 
 const createChart = (currency, data) => {
 
+/*  Funcion encargada de procesar los datos entregados por la api "mi indicador" y renderizar un grafico de líneas e inyectarlo en el DOM.
+
+    Recordar que el parámetro "data" es un arreglo de objetos, 30 en particular ya que eso es lo que de vuelve la api. Cada objeto se 
+    compone de 2 elementos: fecha y valor por tanto, lo primero es separar ambos tipos de datos ya que las fechas actuarán como etiquetas
+    (eje X) del gráfico y los valores serán desplegados en el eje Y
+    
+    Haciendo uso de un map, se recorre el arreglo de objetos para extraer las fechas. En cada iteración, se ejecutará una función que retornará
+    el valor de fecha de cada objeto no sin antes haberlo "trasnformado" (haciendo uso del split). Esto por que el formto de fecha del objeto
+    es: "2024-02-12T03:00:00.000Z" y lo que nos interesa es unicamente la fecha en formato aaaa-mm-dd.
+
+    El map devuelve entonces un arreglo con las fechas en el formato deseado.*/
+
     const fechasTemp = data.map((dato) => {
         const fecha = dato.fecha.split("T")[0];
         return fecha;
     });
+
+/*  Se procede de la misma manera para extraer los valores asociados a cada fecha. haciendo uso del metodo Number y parsefloat se toman datos 
+    numericos en punto flotante.*/
 
     const valoresTemp = data.map((dato) => {
         const valor = dato.valor;
         return Number(parseFloat(valor).toFixed(3));
     });
 
+/*  Se aplica el metodo "reverse" puesto que la api ordena los datos desde el mas nuevo al mas antiguo, lo que no se entiende claramente 
+    en un grafico, donde los datos van desde el mas antiguo al mas nuevo. Por otra parte, según solicita el desafío, se aprovecha la 
+    instancia para tomar solo los primeros 10 datos de fechas y valores desde el arreglo original.
+
+    Así, los valores a graficar serán "labels" y "values".*/
+
     const labels = fechasTemp.splice(0,10).reverse();
     const values = valoresTemp.splice(0,10).reverse();
 
+/*  Para poder crear un grafico tenemos que utilizar el metodo "new Chart" el cual recibe dos parámetros:
+
+    - El contexto de renderizado, sus propiedades y sus funciones. Esto se obtiene haciendo uso del metodo getContext('2d') a la tag
+    <canvas> del DOM. Al trabajar con gráficas en 2D se debe indicar que es este el tipo de contexto. Esto se hace ingresando el parámetro '2D'.*/
+    
     ctx = document.getElementById("myChart").getContext('2d');
    
     const datasets = [
         {
             label: "Valor "+ currency +" - 10 últimos días",         //Titulo del grafico
-            data: values                                    // Valores (puntos), eje Y
+            data: values                                            // Valores (puntos), eje Y
         }
     ];
+
+/*  Como segundo parámetro, se debe entregar un objeto que contendrá información de configuración del tipo de grafico y la data a renderizar.*/
 
     const chartData = {labels, datasets}                    // Labels corresponde a arreglo de fechas (eje X). Datasets corresponde a arreglo que contiene el titulo 
                                                             // del grafico y valores (puntos) del eje Y.
@@ -138,7 +166,16 @@ const createChart = (currency, data) => {
         type: "line",                                       // se indica el tipo de grafico y la información (data) para renderizar el gráfico.
         data: chartData
     };
-  
+
+/*  Antes de crear el grafico, revisamos si ya existe una instancia creada y si existe, se elimina mediante el metodo destroy(). Esto porque al momento en que 
+    querramos renderizar un nuevo grafico, al cambiar de tipo de moneda, el plugin indicará un error que hace mención a que la instancia previamente creada debe
+    ser destruida antes de crear un nuevo grafico:
+
+    Error: Canvas is already in use. Chart with ID '0' must be destroyed before the canvas with ID 'myChart' can be reused.
+
+    Por tanto, si myChart es True, quiere decir que ya existe una instancia creada y la destruye para luego proceder a crear una nueva instancia asociaa a la 
+    variable myChart.*/
+
     if(myChart){
         myChart.destroy();
     }
