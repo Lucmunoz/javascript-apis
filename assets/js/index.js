23de659ec58fadb72f1476051ce4f553d0fb6059
lucmunoz <lucas.munoz.vasquez@gmail.com>
2024-02-14 22:21:44 -0300

    cambios

--- assets/js/index.js
+++ assets/js/index.js
@@ -64,7 +64,6 @@ let ctx = document.getElementById("myChart").getContext('2d');
     para deconstruir un objeto es: " const {identificador} = objeto; ", donde identificado es el nombre de la propiedad a extraer o acceder del objeto. Luego de 
     deconstruir la variable identificador, contiene el valor de dicha propiedad.
 
-<<<<<<< HEAD
     Lo anterior, es algo simiar a la siguiente expresión "const identificador = objeto.identificador".
 */
 
@@ -77,88 +76,11 @@ const getData = async(currency)=>{
 
 /* */
 const buttonPress = async (currency) => {
-=======
-    try {
-        const res = await fetch("https://mindicador.cl/api/"+divisa+"/");
-
-            /*  A continuación, accederemos a los datos utilizando el metodo .json(). Podemos ejecutar la siguiente líne de codigo:
-            
-                const data = await res.json(); //(1)
-                console.log(data)
-
-                Podrémos ver entonces que el json devuelve un objeto con la siguiente estructura
-
-                {
-                    "version": "1.7.0",
-                    "autor": "mindicador.cl",
-                    "codigo": "dolar",
-                    "nombre": "Dólar observado",
-                    "unidad_medida": "Pesos",
-                    "serie": [
-                        {
-                            "fecha": "2024-02-13T03:00:00.000Z",
-                            "valor": 972.22
-                        },
-                        
-                        ... // son 31 elementos por la forma en la que hicimos el fetch. Al utilizar una URL que indique el tipo de divisa, la api devuelve los daltos del último mes (últimos 31 días)
-                        
-                        {
-                            "fecha": "2024-01-02T03:00:00.000Z",
-                            "valor": 877.12
-                        }
-                    ]
-                }
-
-                Del objeto anterior, lo único que nos interesa es el arreglo de objetos "serie" y particularmente, el primer elemento (indice [0]) ya que este corresponde al último valor vigente (valor al día)
-                de la divisa seleccionada por el usuario. 
-
-                Así, para obtener este arreglo, podemos ejecutar la siguiente línea de codigo.
-                */
-
-            const {serie} = await res.json();
-            
-            /*  Luego de lo anterior, ya no tendríamos como resultado un objeto tan grande y con datos que no nos interesan sino que obtenemos directamente el arreglo series (que es un ARREGLO de OBJETOS)
-
-            [
-                {
-                    "fecha": "2024-02-13T03:00:00.000Z",
-                    "valor": 972.22
-                },
-                
-                ...,
-
-                {
-                    "fecha": "2024-01-02T03:00:00.000Z",
-                    "valor": 877.12
-                }
-            ]
-
-            A partir de lo anterior, y dado que el cálculo de conversión de moneda debe hacerse con el valor vigente de la moneda seleccionada, tenemos que obtener el valor del elemento "valor" del primer 
-            objeto yaque los objetos se ordenan {mas reciente,....,mas antiguo}. Esto lo hacemos con la siguiente línea de codigo.*/
-
-            valorDivisa= Number(parseFloat(serie[0].valor)); 
-         
-            /*  Si hacemos un console.log del valor de la variable "valorDivisa" obtendríamos directamente el valor de la divisa para la última fecha entregada por la api. En el caso del dolar, al 13 de febrero
-            obtendríamos en pantalla "972.22" 
-
-            Luego de obtener este valor, llamamos a la función encargada de hacer la conversión.
-            */
-                   
-        convertData(valorDivisa);
->>>>>>> 7b1c389c5463bd0b8a4551accf21823ecc05b192
 
     try{
         const data = await getData (currency)
 
-<<<<<<< HEAD
         convertAndShowCurrency(data)
-=======
-        $moneyAmountInputBox.value="";
-        $moneyAmountInputBox.placeholder = "monto en pesos chilenos (CLP)";
-        $convertedAmountText.innerHTML  = 'Error al intentar obtenr información de divisas. Intente mas tarde';
-        $convertedAmountText.className = 'alert';
-        alert("Error. Api mindicador.cl caída");
->>>>>>> 7b1c389c5463bd0b8a4551accf21823ecc05b192
     }
     catch{
     }
@@ -166,92 +88,28 @@ const buttonPress = async (currency) => {
 
 const convertAndShowCurrency = (data) =>{
 
-<<<<<<< HEAD
     let valorIngresado = parseInt($moneyAmountInputBox.value);
     let valorDivisa = data[0].valor;
-=======
-function convertData(divisa){
->>>>>>> 7b1c389c5463bd0b8a4551accf21823ecc05b192
 
 
-<<<<<<< HEAD
     if(isNaN(valorIngresado)) {
         alert("Porfavor, debe ingresar un valor numerico válido. No utilice letras ni puntos.");
-=======
-    let valorTemp = parseInt($moneyAmountInputBox.value);
-
-    if(isNaN(valorTemp)) {
-
->>>>>>> 7b1c389c5463bd0b8a4551accf21823ecc05b192
         $moneyAmountInputBox.value="";
         $moneyAmountInputBox.placeholder = "monto en pesos chilenos (CLP)";
-        $convertedAmountText.innerHTML  = 'Ingrese un Valor numérico, reintente.';
-        $convertedAmountText.className = 'alert';
-        alert("Porfavor, debe ingresar un valor numerico válido. No utilice letras ni puntos.");
+        $convertedAmountText.innerHTML =  "...";
         return;
       }
 
-<<<<<<< HEAD
     valorResultado = parseFloat(valorIngresado/valorDivisa).toFixed(3); 
     $convertedAmountText.innerHTML =  "Resultado: " + valorResultado;
 
     createChart($moneyTypeSelectBox.value, data);
-=======
-    /*  Si el valor es válido, se realiza el calculo de conversión el cual se inyecta al HTML */
-    valorTemp = parseFloat(valorTemp/divisa).toFixed(3); 
-    $convertedAmountText.className = '';
-    $convertedAmountText.innerHTML =  "Resultado: " +valorTemp;
->>>>>>> 7b1c389c5463bd0b8a4551accf21823ecc05b192
 }
 
 
 const createChart = (currency, data) => {
 
-<<<<<<< HEAD
     const fechasTemp = data.map((dato) => {
-=======
-    /*  Función cuya ejecución se encuentra asociada al evento change del elemento select en donde el usuario elige el tipo de moneda a convertir. Al momento en
-        que el usuario elige una nueva opción se invoca esta función y se entrega como parametro el tipo de moneda seleccionada.
-        
-        con fetch realizamos una peteición a la API mindicador. Este método devuelve una promesa y para gestionarla, utilizaremos await. Con este metodo, se detiene
-        la ejecución del código y no continua. Asociado al await, tenemos que agregar la palabra reservada "async" antes de la palabra function
-
-        con await esperamos a que se complete la solicitud de manera forzada.
-        
-        */
-
-    try {
-        const res = await fetch("https://mindicador.cl/api/"+tipo+"/");
-        
-        /*  Al hacer un fetch con el URL indicando el tipo de divisa, json devuelve objeto el cual contiene un arreglo de objetos llamado "serie" que contiene
-            información de la divisa para los ultimos 30 días. Cada objeto tiene 2 elementos: "fecha" y "valor". Ambos datos son de interes para poder renderizar
-            un gráfico fecha v/s valor por lo que extraemos este arreglo desde el objeto*/
-
-        const {serie} = await res.json();        
-        createChart(serie); // Función encargada de crear el gráfico. Se entrega como parámetro el arreglo "serie"
-    }
-    catch (error) {
-        alert("Error. Api caída");
-    }
-}
-
-function createChart(datos){
-    
-    /*  Funcion encargada de procesar los datos entregados por la api "mi indicador", renderizar un grafico de líneas e inyectarlo en el DOM.
-   
-        Recordar que el parámtro "datos" es un arreglo de objetos, 30 en particular ya que eso es lo que de vuelve la api. Cada objeto se compone de 
-        2 elementos: fecha y valor por tanto, lo primero es separar ambos tipos de datos ya que las fechas actuarán como etiquetas (eje X) del gráfico y 
-        los valores serán desplegados en el eje Y
-        
-        Haciendo uso de un map, se recorre el arreglo de objetos. En cada iteración, se ejecutará una función que retornará el valor de fecha de cada objeto
-        no sin antes haberlo "adaptado" (haciendo uso del split). Esto por que el formto de fecha del objeto es: "2024-02-12T03:00:00.000Z" y lo que nos
-        interesa es unicamente la fecha en formato aaaa-mm-dd.
-
-        El map devuelve entonces un arreglo con las fechas en el formato deseado.
-     */
-
-    const labelsTemp = datos.map((dato) => {
->>>>>>> 7b1c389c5463bd0b8a4551accf21823ecc05b192
         const fecha = dato.fecha.split("T")[0];
         return fecha;
     });
@@ -268,13 +126,8 @@ function createChart(datos){
    
     const datasets = [
         {
-<<<<<<< HEAD
             label: "Valor "+ currency +" - 10 últimos días",         //Titulo del grafico
             data: values                                    // Valores (puntos), eje Y
-=======
-            label: "Valor dolar - 10 últimos días",         //Titulo del grafico
-            data: values,                                    // Valores (puntos), eje Y
->>>>>>> 7b1c389c5463bd0b8a4551accf21823ecc05b192
         }
     ];
 
@@ -284,25 +137,15 @@ function createChart(datos){
     const config = {                                        // Objeto de configuración
         type: "line",                                       // se indica el tipo de grafico y la información (data) para renderizar el gráfico.
         data: chartData
-        
     };
   
     if(myChart){
         myChart.destroy();
-        
     }
     
     $chart.classList.add("white-background");
     myChart = new Chart(ctx, config);
-
 }
 
 
     
-
-    
-
-    
-
-
-
